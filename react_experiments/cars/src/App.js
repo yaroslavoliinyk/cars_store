c51f73a87466d2716840b5b6da6ecb331fe4ca42
yaroslavoliinyk <y.oliyn7@gmail.com>
2023-11-28 22:51:29 +0200

    feat: add my first component

--- react_experiments/cars/src/App.js
+++ react_experiments/cars/src/App.js
@@ -1,16 +1,28 @@
 // import logo from './logo.svg';
 // import './App.css';
+import Header from "./components/Header"
 
 function App() {
-  let data = ["Fiat", "Peugeot","Ford","Renault","Citroen"]
+  let data = [
+    {brand:"Fiat", color:"green", model:"500L",price:7000, "year":2020,"id":1},
+    {brand:"Peugeot", color:"red", model:"5008", price:8000, "year":2018,"id":2},
+    {brand:"Volkswagen", color:"white", model:"Golf 7", price:8500, "year":2019,"id":3},
+    {brand:"Fiat", color:"green", model:"Tipo", price:10000, "year":2019,"id":4},
+    {brand:"Kia", color:"black", model:"Ceed", price:6000, "year":2010,"id":5},
+    {brand:"Volkswagen", color:"white", model:"Golf 7", price:8500, "year":2019,"id":15},
+    {brand:"Fiat", color:"gray", model:"Ritmo", price:300, "year":1990,"id":21}
+  ]
   return (
     <div className="App max-w-3xl mx-auto h-full">
-     <h1 className="bg-slate-500 text-white text-center">This is a Tailwind styled site!</h1>
+      <Header/>
       <div>
         {data.map(
           (el)=>{
-            return <div>Cars listed as <span className=
-              "font-bold">{el.toUpperCase()}</span></div>
+            return (<div key={el.id}>
+              <span className="font-bold">
+                {el.brand.toUpperCase()}</span>
+                </div>
+                )
           } )}
       </div>
     </div>
---
+++ react_experiments/cars/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="flex flex-row bg-orange-600 text-white align-middle justify-center p-5">
+        <h1>Cars Sales App</h1>
+    </div>
+  )
+}
+
+export default Header
